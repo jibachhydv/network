78c223e18a5c86b5c1e0892879103192a4845725
jibachh yadav <jibachhydv18@gmail.com>
2021-07-11 18:30:26 +0545

    Profile model created

--- profileApp/models.py
+++ profileApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+
+    # User
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile',)
+
+    # Profile Picture
+    profile_picture = models.ImageField(verbose_name="Profile picture")
+
+    # DOB
+    dob = models.DateField(verbose_name="Date of Birth")
+
+    # school name
+    school_name = models.CharField(verbose_name="School Name")
+
+    def __str__(self):
+        return f"Profile of {self.user}"
