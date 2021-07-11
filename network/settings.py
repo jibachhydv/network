61763c0c93b5453f9f0c3739d3cbccc19a329788
jibachh yadav <jibachhydv18@gmail.com>
2021-07-11 14:12:49 +0545

    authApp registered

--- network/settings.py
+++ network/settings.py
@@ -25,6 +25,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'authApp',
+
+    # Third Party Apps
 ]
 
 MIDDLEWARE = [
