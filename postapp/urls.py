1795d6c8553861351dd33489db51d3026e81d1ee
jibachh yadav <jibachhydv18@gmail.com>
2021-07-13 00:00:37 +0545

    many modification

--- postapp/urls.py
+++ postapp/urls.py
@@ -1,6 +1,7 @@
-from .views import PostList
+from .api.views import PostList, PostDetail
 from django.urls import path
 
 urlpatterns = [
-    path("", PostList.as_view()),
+    path("", PostList.as_view(), name="post_lists"),
+    path("<int:pk>", PostDetail.as_view(), name="post_detail"),
 ]
