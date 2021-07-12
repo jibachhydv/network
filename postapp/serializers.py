20b52ea65b2365eb24c0116e62660c4c93ed1e63
jibachh yadav <jibachhydv18@gmail.com>
2021-07-12 22:35:28 +0545

    Serializer file added

--- postapp/serializers.py
+++ postapp/serializers.py
@@ -1,1 +1,23 @@
-# POST Serializers
+from rest_framework import serializers
+from .models import Post, Comment, LikeModel
+
+
+class PostSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        models = Post
+        fields = "__all__"
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = "__all__"
+        
+
+class LikeSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = LikeModel
+        fields = "__all__"
