08ca8da81938fb610955854f2faba5e8c9c87253
jibachh yadav <jibachhydv18@gmail.com>
2021-07-12 18:12:47 +0545

    djoser setting updated

--- network/urls.py
+++ network/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^auth/', include("djoser.urls")),
+    url(r'^auth/', include('djoser.urls.jwt')),
+    url(r'^auth/', include('djoser.urls.authtoken')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
